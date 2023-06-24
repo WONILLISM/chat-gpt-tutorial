4ec24048b73cfb60719a0735cace5e79718203e5
WONILLISM <wonillism@gmail.com>
2023-06-24 13:54:14 +0900

    init question control component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,24 @@
+import { useState } from "react";
+
 function App() {
-  return <></>;
+  const [question, setQuestion] = useState<string>("");
+
+  return (
+    <>
+      <div>
+        <div>
+          <label>Question </label>
+          <input
+            type="text"
+            onChange={(e) => {
+              setQuestion(e.target.value);
+            }}
+          />
+        </div>
+        <button>질문하기</button>
+      </div>
+    </>
+  );
 }
 
 export default App;
